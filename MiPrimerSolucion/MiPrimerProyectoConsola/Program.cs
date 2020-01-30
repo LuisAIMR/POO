0ccb4d446f12d68ec462ab0ed5eddb478347d6f8
LuisAntonioIsmaelMezaRosales <meza75880@gmail.com>
2020-01-30 13:25:15 -0700

    hoola

---
+++ MiPrimerSolucion/MiPrimerProyectoConsola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyectoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("introduce un numero: ");
+            a = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("introduce un numero: ");
+            b = Convert.ToUInt32 (Console.ReadLine());
+
+            c = a + b;
+            Console.ReadKey();
+        }
+    }
+}
